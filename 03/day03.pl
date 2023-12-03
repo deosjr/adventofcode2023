65db7028ef02678e8527c43d714b38c359d933de
Sjoerd Dost <deosjr@gmail.com>
2023-12-03 10:05:06 +0100

    remove spurious reif

--- 03/day03.pl
+++ 03/day03.pl
@@ -1,27 +1,28 @@
 :- use_module(library(charsio)).
-:- use_module(library(reif)).
 
 run_day(3, Filename) :-
     phrase_from_file(parse(0,0), Filename),
     findall(N-X-Z-Y, number(N, X-Z, Y), Numbers),
-    tfilter(part_number, Numbers, PartNumbers),
-    maplist(num, PartNumbers, Nums),
-    sum(Nums, #=, Ans1),
+    maplist(part_number, Numbers, PartNumbers),
+    sum(PartNumbers, #=, Ans1),
     write_part1(Ans1),
     findall(X-Y, symbol('*', X, Y), GearSymbols),
     maplist(gear_ratio, GearSymbols, Ratios),
     sum(Ratios, #=, Ans2),
     write_part2(Ans2).
 
-num(N-_-_-_, N).
-part_number(P, T) :-
-    ( (symbol(_,SX,SY), adjacent(P,SX-SY)) -> T = true ; T = false ).
-
 adjacent(_-FromX-ToX-Y, SX-SY) :-
     SX #>= FromX - 1,
     SX #=< ToX + 1,
     SY #=< Y+1,
     SY #>= Y-1.
+
+% if P is not a proper part number, N = 0
+part_number(P, N) :-
+    ( (symbol(_,SX,SY), adjacent(P,SX-SY)) -> 
+        P = N-_-_-_
+        ; N #= 0
+        ).
 
 % if X-Y is not a proper gear, Ratio = 0
 gear_ratio(X-Y, Ratio) :-
