a16bb9ebca7f0d8134cc917fec62dd27efcb3ce4
Sjoerd Dost <deosjr@gmail.com>
2023-12-06 09:30:38 +0100

    day06p2

--- 06/day06.pl
+++ 06/day06.pl
@@ -1,26 +1,34 @@
 :- use_module(library(between)).
 :- use_module(library(charsio)).
 :- use_module(library(lambda)).
-:- use_module(library(reif)).
-
-%use_test_input.
 
 run_day(6, Filename) :-
     phrase_from_file(parse(Times, Distances), Filename),
     maplist(\X^Y^Z^(Z=X-Y), Times, Distances, Zipped),
-    maplist(part1, Zipped, Ways),
+    maplist(solve, Zipped, Ways),
     foldl(\X^Y^Z^(Z#=X*Y), Ways, 1, Ans1),
-    write_part1(Ans1).
+    write_part1(Ans1),
+    phrase_from_file(parse(Time-Distance), Filename),
+    solve(Time-Distance, Ans2),
+    write_part2(Ans2).
 
-part1(T-D, L) :-
-    numlist(0, T, Millis),
-    maplist(D+\X^Y^(Y#=X*(T-X)), Millis, Ds),
-    tfilter(beats(D), Ds, Wins),
-    length(Wins, L).
+% we are guaranteed at least one way to win each race
+% list of distances after calculation is symmetric
+solve(T-D, L) :-
+    solve(T, D, 0, L).
 
-beats(X, Y, T) :- ( Y #>X -> T = true ; T = false ).
+solve(Time, Distance, Charge, Ways) :-
+    Millis #= (Time-Charge) * Charge,
+    ( Millis #> Distance ->
+        Ways #= Time + 1 - (2 * Charge) 
+        ;
+        N #= Charge + 1,
+        solve(Time, Distance, N, Ways)
+    ).
 
 parse(T,D) --> "Time:", intlist(T), "\nDistance:", intlist(D), "\n".
+
+parse(T-D) --> "Time:", int_with_spaces(T), "\nDistance:", int_with_spaces(D), "\n".
 
 intlist([N|T]) --> spaces, integer(N), intlist(T).
 intlist([]) --> [].
@@ -31,3 +39,8 @@ spaces --> [].
 integer(N) --> digits(D), {number_chars(N, D)}.
 digits([H|T]) --> [H], {char_type(H, decimal_digit)}, digits(T).
 digits([X]) --> [X], {char_type(X, decimal_digit)}.
+
+int_with_spaces(N) --> digits_with_spaces(D), {number_chars(N, D)}.
+digits_with_spaces(T) --> " ", digits_with_spaces(T).
+digits_with_spaces([H|T]) --> [H], {char_type(H, decimal_digit)}, digits_with_spaces(T).
+digits_with_spaces([X]) --> [X], {char_type(X, decimal_digit)}.
