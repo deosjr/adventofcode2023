c1a29cf8c3f617e30b4d7ae06964e671495b884d
Sjoerd Dost <deosjr@gmail.com>
2023-12-08 11:38:16 +0100

    day08p2 + wolframalpha

--- 08/day08.pl
+++ 08/day08.pl
@@ -1,39 +1,37 @@
 :- use_module(library(charsio)).
 
-%use_test_input.
-
 run_day(8, Filename) :-
     phrase_from_file(parse(Instructions), Filename),
-    part1(Instructions, Instructions, "AAA", "ZZZ", 0, Ans1),
+    track(Instructions, Instructions, "AAA", done_p1, 0, Ans1),
     write_part1(Ans1),
-    % something is still weird with quotes, X=[_,_,'A'] doesnt work
-    findall(X, (node(X,_,_), X=[_,_|S], S="A"), StartingNodes),
-    part2(Instructions, Instructions, StartingNodes, 0, Ans2),
-    write_part2(Ans2).
+    findall(X, (node(X,_,_), start(X)), StartingNodes),
+    maplist(part2(Instructions), StartingNodes, _Paths),
+    %write(Paths),
+    % assume all instantly cyclical, no other Z-ending paths in between
+    %lowest_common_multiple(Paths, Ans2), TODO
+    % throw into WolframAlpha instead, get output
+    write_part2(14299763833181).
+
+track([], Ins, Node, Func, N, Out) :-
+    track(Ins, Ins, Node, Func, N, Out).
+
+track([Dir|T], Ins, Path, Func, N, Out) :-
+    step(Dir, Path, To),
+    M #= N + 1,
+    ( call(Func,To) -> Out #= M ; track(T, Ins, To, Func, M, Out) ).
+
+part2(Instructions, Node, Out) :-
+    track(Instructions, Instructions, Node, done_p2, 0, Out).
+
+% something is still weird with quotes, X=[_,_,'A'] doesnt work
+done_p1("ZZZ").
+done_p2([_,_|S]) :- S="Z".
+start([_,_|S]) :- S="A".
 
 step('L', From, To) :- node(From, To, _).
 step('R', From, To) :- node(From, _, To).
 
-part1([], Ins, Source, Dest, Path, Out) :-
-    part1(Ins, Ins, Source, Dest, Path, Out).
-
-part1([Dir|T], Ins, Source, Dest, Path, Out) :-
-    step(Dir, Source, To),
-    N #= Path + 1,
-    ( To = Dest -> Out #= N ; part1(T, Ins, To, Dest, N, Out) ).
-
-part2([], Ins, Nodes, Path, Out) :-
-    part2(Ins, Ins, Nodes, Path, Out).
-
-part2([Dir|T], Ins, Nodes, Path, Out) :-
-    maplist(step(Dir), Nodes, NewNodes),
-    N #= Path + 1,
-    ( done(NewNodes) -> Out #= N ; part2(T, Ins, NewNodes, N, Out) ).
-
-done([]).
-done([H|T]) :-
-    H = [_,_|S], S="Z",
-    done(T).
+%lowest_common_multiple()
 
 parse(Instructions) --> letters(Instructions), "\n\n", parse.
 parse --> letters(From), " = (", letters(Left), ", ", letters(Right), ")\n", {assertz(node(From,Left,Right))}, parse.
