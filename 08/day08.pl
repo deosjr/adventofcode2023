e84ce0e8ceb511cd48aed6e4ad260b95cf8361bb
Sjoerd Dost <deosjr@gmail.com>
2023-12-08 12:47:59 +0100

    now without wolframalpha

--- 08/day08.pl
+++ 08/day08.pl
@@ -5,12 +5,10 @@ run_day(8, Filename) :-
     track(Instructions, Instructions, "AAA", done_p1, 0, Ans1),
     write_part1(Ans1),
     findall(X, (node(X,_,_), start(X)), StartingNodes),
-    maplist(part2(Instructions), StartingNodes, _Paths),
-    %write(Paths),
+    maplist(part2(Instructions), StartingNodes, Paths),
     % assume all instantly cyclical, no other Z-ending paths in between
-    %lowest_common_multiple(Paths, Ans2), TODO
-    % throw into WolframAlpha instead, get output
-    write_part2(14299763833181).
+    foldl(lcm, Paths, 1, Ans2),
+    write_part2(Ans2).
 
 track([], Ins, Node, Func, N, Out) :-
     track(Ins, Ins, Node, Func, N, Out).
@@ -31,7 +29,11 @@ start([_,_|S]) :- S="A".
 step('L', From, To) :- node(From, To, _).
 step('R', From, To) :- node(From, _, To).
 
-%lowest_common_multiple()
+lcm(X,Y,Z) :- Abs #= abs(X*Y), gcd(X,Y,G), Z #= Abs // G.
+
+gcd(X,X,X).
+gcd(X,Y,G) :- X#<Y, Y1 #= Y-X, gcd(X,Y1,G).
+gcd(X,Y,G) :- X#>Y, gcd(Y,X,G).
 
 parse(Instructions) --> letters(Instructions), "\n\n", parse.
 parse --> letters(From), " = (", letters(Left), ", ", letters(Right), ")\n", {assertz(node(From,Left,Right))}, parse.
