7360fd23fd805d8ab5ec8a4d62e6c1260e196f60
Sjoerd Dost <deosjr@gmail.com>
2023-12-13 17:37:11 +0100

    day13p2

--- 13/day13.pl
+++ 13/day13.pl
@@ -1,25 +1,38 @@
-:- use_module(library(lists)).
-
 run_day(13, Filename) :-
     phrase_from_file(parse(List), Filename),
-    maplist(find_mirror, List, Ns),
+    maplist(find_mirror(prefix), List, Ns),
     sum(Ns, #=, Ans1),
-    write_part1(Ans1).
+    write_part1(Ans1),
+    maplist(find_mirror(prefix_diff(1)), List, Ms),
+    sum(Ms, #=, Ans2),
+    write_part2(Ans2).
 
-find_mirror(List, N) :-
-    ( find_mirror(List, [], X) -> N #= 100*X
-    ; transpose(List, T), find_mirror(T, [], N) ).
+find_mirror(Pred, List, N) :-
+    ( find_mirror(Pred, List, [], X) -> N #= 100*X
+    ; transpose(List, T), find_mirror(Pred, T, [], N) ).
 
-find_mirror([A,B|T], List, N) :-
-    A \= B, find_mirror([B|T], [A|List], N).
-find_mirror([X,X|T], List, N) :-
-    ( is_mirror(List, T) -> length([X|List], N) ; find_mirror([X|T], [X|List], N) ).
+find_mirror(Pred, [A,B|T], List, N) :-
+    ( is_mirror(Pred, [A|List], [B|T]) -> length([A|List], N) ; find_mirror(Pred, [B|T], [A|List], N) ).
 
-is_mirror(List, T) :-
-    prefix(List, T) ; prefix(T, List).
+is_mirror(Pred, List, T) :-
+    call(Pred, List, T) ; call(Pred, T, List).
 
 prefix(Prefix, List) :-
     append(Prefix, _, List).
+
+prefix_diff(0, [], _).
+prefix_diff(N, [X|A], [Y|B]) :-
+    diff(X, Y, Z),
+    prefix_diff(M, A, B),
+    N #= M + Z.
+
+diff([],[], 0).
+diff([X|A], [X|B], N) :-
+    diff(A, B, N).
+diff([X|A], [Y|B], N) :-
+    X \== Y,
+    diff(A, B, M),
+    N #= M+1.
 
 parse([P|T]) --> pattern(P), "\n", parse(T).
 parse([P]) --> pattern(P).
