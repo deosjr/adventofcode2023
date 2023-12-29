b63ea5a6de85c6898576ac3403a9bcd55a288b78
Sjoerd Dost <deosjr@gmail.com>
2023-12-29 09:21:42 +0100

    day22p1 fast enough

--- 22/day22.pl
+++ 22/day22.pl
@@ -1,3 +1,4 @@
+:- use_module(library(assoc)).
 :- use_module(library(between)).
 :- use_module(library(charsio)).
 :- use_module(library(lambda)).
@@ -7,25 +8,27 @@
 run_day(22, Filename) :-
     phrase_from_file(parse(Bricks), Filename),
     settle_bricks(Bricks, Settled),
+    %write(Settled),nl,
     safe_disintegrate(Settled, Ans1),
     write_part1(Ans1).
 
 settle_bricks(Bricks, Settled) :-
     qsort(byLowZ, Bricks, Sorted),
-    settle_bricks(Sorted, [], Settled).
+    empty_assoc(Assoc),
+    settle_bricks(Sorted, Assoc, Settled).
 
-settle_bricks([], X, X).
-settle_bricks([Brick|T], Settled, Total) :-
+settle_bricks([], _, []).
+settle_bricks([Brick|T], Assoc, [NewSettled|Settled]) :-
     %write(Brick),nl,
-    settle(Brick, Settled, NewSettled),
-    settle_bricks(T, NewSettled, Total).
+    settle(Brick, Assoc, NewSettled),
+    update_assoc(NewSettled, Assoc, Assoc0),
+    settle_bricks(T, Assoc0, Settled).
 
-settle(Brick, Settled, Out) :-
-    intersect(Brick, Settled, Z),
+settle(Brick, Assoc, NewBrick) :-
+    intersect(Brick, Assoc, Z),
     Brick = (X1-Y1-Z1)/(X2-Y2-Z2),
     ZZ #= Z2 - (Z1 - Z),
-    NewBrick = (X1-Y1-Z)/(X2-Y2-ZZ),
-    qsort(byHighZ, [NewBrick|Settled], Out).
+    NewBrick = (X1-Y1-Z)/(X2-Y2-ZZ).
 
 % lines through each X-Y pixel of brick downwards
 rays((X1-Y-Z)/(X2-Y-Z), Rays) :-
@@ -40,20 +43,28 @@ rays((X-Y-_)/(X-Y-_), [X-Y]).
 
 % find Z where brick settles; highest intersecting Z-value + 1 or 1 if none (which is on the floor)
 % assumption: rays start higher than any potential intersecting brick (otherwise there'd be intersections in input)
-intersect(Brick, Settled, Z) :-
+intersect(Brick, Assoc, Z) :-
     rays(Brick, Rays),
-    intersect(Rays, Settled, Z).
+    maplist(intersect(Assoc), Rays, Zs),
+    sort(Zs, Sorted),
+    reverse(Sorted, [Z|_]).
 
-intersect(_, [], 1).
-intersect(Rays, [Brick|Settled], Z) :-
-    ( brick_intersect(Brick, Rays, ZZ) -> Z #= ZZ ; intersect(Rays, Settled, Z) ).
+intersect(Assoc, Ray, Z) :-
+    ( get_assoc(Ray, Assoc, ZZ) -> Z #= ZZ+1 ; Z #= 1 ). 
 
-brick_intersect(Brick, Rays, Z) :-
-    rays(Brick, BrickRays),
-    member(Ray, Rays),
-    member(Ray, BrickRays),
-    Brick = _/(_-_-ZZ),
+% B1 falling, B2 supporting
+brick_intersect(B1, B2, Z) :-
+    rays(B1, R1),
+    rays(B2, R2),
+    member(R, R1),
+    member(R, R2),
+    B2 = _/(_-_-ZZ),
     Z #= ZZ+1.
+
+update_assoc(Brick, Assoc, Assoc0) :-
+    Brick = _/(_-_-Z),
+    rays(Brick, Rays),
+    foldl(Z+\A^B^C^(put_assoc(A, B, Z, C)), Rays, Assoc, Assoc0).
 
 safe_disintegrate(Settled, Ans) :-
     findall(Z, member(_/(_-_-Z),Settled), HighZs),
@@ -66,9 +77,8 @@ safe_disintegrate([Z|ZT], Bricks, Acc, Ans) :-
     ZZ #= Z+1,
     findall(B, (B=_/(_-_-Z), member(B, Bricks)), SupportingBricks),
     findall(B, (B=(_-_-ZZ)/_, member(B, Bricks)), FallingBricks),
-    write(SupportingBricks), write(FallingBricks), nl,
-    maplist(\X^Y^(findall(F, (member(F, FallingBricks), intersect(F, [X], ZZ)), Fs), Y=X-Fs), SupportingBricks, BFs),
-    maplist(\X^Y^(X=B-Fs, write(B-Fs),nl, (maplist(\Q^(member(R,SupportingBricks),R\=B,intersect(Q,[R],ZZ)), Fs) -> Y#=1 ; Y#= 0)), BFs, Nums),
+    maplist(\X^Y^(findall(F, (member(F, FallingBricks), brick_intersect(F, X, ZZ)), Fs), Y=X-Fs), SupportingBricks, BFs),
+    maplist(\X^Y^(X=B-Fs, (maplist(\Q^(member(R,SupportingBricks),R\=B,brick_intersect(Q,R,ZZ)), Fs) -> Y#=1 ; Y#= 0)), BFs, Nums),
     sum(Nums, #=, L),
     NAcc #= Acc + L,
     safe_disintegrate(ZT, Bricks, NAcc, Ans).
